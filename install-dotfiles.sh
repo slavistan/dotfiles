9486a4c4b0c09dd454c589ee25b5bf01bae3fa69
Stanislaw Huell <huell@stud.uni-heidelberg.de>
2019-05-18 17:35:29 +0200

    asd

---
+++ install-dotfiles.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+BASEDIR=$(dirname $(realpath "$0"))
+mkdir -p ~/.config
+
+read -rsp 'Enter your sudo password: ' pw
+sudo -kSp '' true <<<"${pw}" > /dev/null 2>&1
+if [[ "$?" != "0" ]]; then
+  echo 'Invalid password. Abort.'
+  exit 1
+fi
+echo 'OK.'
+
+echo "Installing git .. "
+if [[ -z "$(command -v git)" ]]; then
+  sudo -Sp '' yum install git <<<${pw}
+fi
+
+echo "Install cmake .. "
+if [[ -z "$(command -v cmake)" ]]; then
+  sudo -Sp '' yum install cmake <<<${pw}
+fi
+
+echo 'Installing nvim ...'
+if [[ -z "$(command -v nvim)" ]]; then
+  rm -rf ~/Downloads/dotfiles/nvim
+  mkdir -p ~/Downloads/dotfiles/nvim && cd "$_"
+  git clone https://github.com/nvim/nvim.git ~/Downloads/dotfiles/nvim
+  make CMAKE_BUILD_TYPE=Release
+  sudo -Sp '' make install <<<${pw}
+  cd $BASEDIR
+  rm -rf ~/Downloads/dotfiles/nvim
+fi
+mkdir -p ~/.config/nvim/plug-plugins ~/.config/nvim/autoload
+nvim +PlugInstall +quitall
+
+echo 'Installing st ...'
+rm -rf ~/Downloads/dotfiles/st
+mkdir -p ~/Downloads/dotfiles/st && cd "$_"
+git clone git@github.com:slavistan/st.git ~/Downloads/dotfiles/st
+sudo -Sp '' make clean install <<<${pw}
+cd $BASEDIR
+
+echo 'Installing zsh ...'
+rm -rf ~/.zshrc ~/.config/zsh
+mkdir -p ~/.config && cp -r zsh ~/.config
+ln -s ~/.config/zsh/zshrc ~/.zshrc
+
+echo 'Installing i3 ...'
+rm -rf ~/.config/i3
+mkdir -p ~/.config && cp -r i3 ~/.config
+
---
+++ nvim/init.vim
@@ -0,0 +1,58 @@
+let g:NVIMHOME=$HOME . '/.config/nvim'
+
+"""
+" Plugins
+"""
+execute 'call plug#begin(''' . g:NVIMHOME . '/plug-plugins'')'
+Plug 'https://github.com/flazz/vim-colorschemes.git'
+Plug 'https://github.com/airblade/vim-gitgutter.git'
+call plug#end()
+"""
+" Settings
+"""
+
+colorscheme molokai
+
+let &backupdir=g:NVIMHOME . '/backup'
+let &directory=&backupdir
+let &shadafile=&backupdir . '/shada'
+
+set clipboard=unnamed
+set ignorecase
+set smartcase
+set linebreak
+set virtualedit=block
+set path+=**
+set updatetime=100
+set scrolloff=999 " Center cursor vertically
+set number
+let mapleader=","
+
+set list listchars=tab:‣\ ,trail:␣ " Display tabs and trailing whitespaces
+set tabstop=2 softtabstop=2 shiftwidth=2 expandtab
+set autoindent " Keep indentation level for wrapped lines
+set breakindent " Wrapped lines preserve indentation
+
+"""
+" Key mappings
+"""
+nnoremap ; :
+vnoremap ; :
+nnoremap <Leader>yy ^yg_
+nnoremap <Leader>dd ^dg_
+
+" Use regular movement keys to navigate normally across wrapped lines
+noremap <buffer> <silent> k gk
+noremap <buffer> <silent> j gj
+noremap <buffer> <silent> 0 g0
+noremap <buffer> <silent> $ g$
+onoremap <silent> j gj
+onoremap <silent> k gk
+
+" Tab behaviour
+nnoremap <Tab> >>
+nnoremap <S-Tab> <<
+vnoremap <Tab> >gv
+vnoremap <S-Tab> <gv
+inoremap <S-Tab> <Backspace>
+
