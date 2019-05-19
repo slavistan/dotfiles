5d92b646801d8a3ba10f577a01c23fce2c0aa134
Stanislaw Huell <huell@stud.uni-heidelberg.de>
2019-05-19 17:48:26 +0200

    update

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-PKGMGR="$1"
-if [[ -z "$(command -v $PKGMGR)" ]]; then
-  echo 'Unknown package manager.'
-  exit 1
-fi
-
 rm -rf ~/Downloads/dotfiles
 BASEDIR=$(dirname $(realpath "$0"))
 
@@ -19,53 +13,33 @@ if [[ "$?" != "0" ]]; then
 fi
 echo 'OK.'
 
-echo "Installing git .. "
-if [[ -z "$(command -v git)" ]]; then
-  sudo -Sp '' $PKGMGR install -y git <<<${pw}
+reqs=(git cmake nvim i3 i3blocks zsh)
+command -v $reqs
+if [[ "$?" != "0" ]]; then
+  echo "Please install requirements: "$reqs
+  exit 1
 fi
 
-echo "Install cmake .. "
-if [[ -z "$(command -v cmake3)" ]]; then
-  sudo -Sp '' $PKGMGR install -y cmake3 <<<${pw}
-fi
-
-echo 'Installing nvim ...'
-if [[ -z "$(command -v nvim)" ]]; then
-  sudo -Sp '' $PKGMGR install -y libtool <<<${pw}
-  mkdir -p ~/Downloads/dotfiles/nvim && cd "$_"
-  git clone https://github.com/neovim/neovim.git ~/Downloads/dotfiles/nvim
-  make CMAKE_BUILD_TYPE=Release
-  sudo -Sp '' make install <<<${pw}
-  cd $BASEDIR
-fi
+echo "Configuring nvim ..."
 mkdir -p ~/.config/nvim/plug-plugins ~/.config/nvim/autoload
 nvim +PlugInstall +quitall
 
-echo 'Installing st ...'
+echo 'Configuring st ...'
 sudo -Sp '' $PKGMGR install -y fontconfig-devel <<<${pw}
 mkdir -p ~/Downloads/dotfiles/st && cd "$_"
 git clone https://github.com/slavistan/st.git ~/Downloads/dotfiles/st
 sudo -Sp '' make clean install <<<${pw}
 cd $BASEDIR
 
-echo 'Installing zsh ...'
-rm -f ~/.zshrc
-ln -s ~/.config/zsh/zshrc ~/.zshrc
+echo 'Configuring zsh ...'
+ln -fs ~/.config/zsh/zshrc ~/.zshrc
 rm -rf ~/.config/zsh/oh-my-zsh
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.config/zsh/oh-my-zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/oh-my-zsh/plugins/zsh-syntax-highlighting
 git clone --recursive git://github.com/joel-porquet/zsh-dircolors-solarized ~/.config/zsh/oh-my-zsh/plugins/zsh-dircolors-solarized
 echo 'source ~/.zshrc;setupsolarized dircolors.ansi-light' | zsh -s
 
-echo 'Installing i3 ...'
-rm -rf ~/Downloads/dotfiles/i3blocks
-git clone https://github.com/vivien/i3blocks.git ~/.Downloads/dotfiles/i3blocks && cd "$_"
-./autogen.sh
-./configure
-make
-sudo -Sp '' make clean install <<<${pw}
-rm -f ~/.i3blocks.conf
-ln -s ~/.config/i3/i3blocks.conf ~/.i3blocks.conf
+echo 'Configuring i3 ...'
+# Nothing to do
 
 rm -rf ~/Downloads/dotfiles
-
--- zsh/startup.zsh
+++ zsh/startup.zsh
@@ -13,27 +13,27 @@ function chpwd() {
 }
 
 ## <CR> executes default command
-my-accept-line () {
-    ## check if the buffer does not contain any words
-    if [ ${#${(z)BUFFER}} -eq 0 ]; then
-        ## put newline so that the output does not start next to the prompt
-        echo
-        ## check if inside git repository
-        if git rev-parse --git-dir > /dev/null 2>&1 ; then
-            ##if so, execute `git status'
-            git status
-        else
-            ## else run `ls'
-            ls
-        fi
-    fi
-    ## in any case run the `accept-line' widget
-    zle accept-line
-}
-## create a widget from `my-accept-line' with the same name
-zle -N my-accept-line
-## rebind Enter, usually this is `^M'
-bindkey '^M' my-accept-line
+# my-accept-line () {
+#     ## check if the buffer does not contain any words
+#     if [ ${#${(z)BUFFER}} -eq 0 ]; then
+#         ## put newline so that the output does not start next to the prompt
+#         echo
+#         ## check if inside git repository
+#         if git rev-parse --git-dir > /dev/null 2>&1 ; then
+#             ##if so, execute `git status'
+#             git status
+#         else
+#             ## else run `ls'
+#             ls
+#         fi
+#     fi
+#     ## in any case run the `accept-line' widget
+#     zle accept-line
+# }
+# ## create a widget from `my-accept-line' with the same name
+# zle -N my-accept-line
+# ## rebind Enter, usually this is `^M'
+# bindkey '^M' my-accept-line
 
 ## Unbind Ctrl-D
 setopt IGNORE_EOF
