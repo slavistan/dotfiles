0dd08668ff322822d747265761ed261563742767
Stanislaw Hüll <Stanislaw.Huell@js-soft.com>
2019-05-25 13:55:53 +0200

    wintty setup

---
+++ win10/wintty/start-ubuntu-wsltty.bat
@@ -0,0 +1,7 @@
+@echo off
+rem Start mintty terminal for WSL package Ubuntu
+
+set wsltty_bin=C:\Tools\wsltty\bin\mintty.exe
+set config_dir="C:\Users\shuell\projects\dotfiles\win10\wintty"
+
+%wsltty_bin% --WSL="Ubuntu" --configdir=%config_dir% /bin/bash -c 'cd ~;zsh'
