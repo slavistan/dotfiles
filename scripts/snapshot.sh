910ca425d7df73a165779319b5e6afd814deaa89
Stanislaw Hüll <Stanislaw.Huell@js-soft.com>
2019-08-13 16:31:12 +0200

    update

--- scripts/snapshot.sh
+++ scripts/snapshot.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+# TODO: Video-record a screen area
 if [ "$1" = "selection" ]; then
   outfile=$(mktemp --tmpdir=/tmp --suffix=.png snapshot_XXXXX)
   gnome-screenshot --file=$outfile --area
