985b78511f9e22959c7051495c06823628b197de
Stanislaw Hüll <Stanislaw.Huell@js-soft.com>
2019-09-16 13:38:04 +0200

    update

--- scripts/ffreload.sh
+++ scripts/ffreload.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+# Reloads firefox's current tab by emulating key presses. Used for reloading
+# html pages on file change.
+#
+# firefox "/my/source/file.html"
+# echo "/my/source/file.html" | entr -ps "/this/script.sh"
 current_wid=$(xdotool getactivewindow)
 ff_wid=$(xdotool search --name "Mozilla Firefox" | head -1)
 xdotool windowactivate $ff_wid
