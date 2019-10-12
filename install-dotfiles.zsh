2f457be249c6ed0cbe8dfe7bb332177892b08132
Stan <st@nislaw.de>
2019-10-12 23:04:41 +0200

    update

--- install-dotfiles.zsh
+++ install-dotfiles.zsh
@@ -54,7 +54,8 @@ setup_prerequisites() {
   libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev \
   libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev \
   libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev \
-  autoconf xutils-dev libtool automake libxcb-xrm0 libxcb-shape0-dev
+  autoconf xutils-dev libtool automake libxcb-xrm0 libxcb-shape0-dev \
+  fonts-powerline dmenu fonts-inconsolata fonts-hack fonts-symbola
   logln '... done setting up prerequisites.'
 }
 
