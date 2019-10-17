70b458a2eea121a98290f60d8c26aaca0afd4e60
Stan <st@nislaw.de>
2019-10-17 11:18:26 +0200

    update

--- scripts/snapshot.sh
+++ scripts/snapshot.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env sh
 
-# TODO: Video-record a screen area
-if [ "$1" = "selection" ]; then
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+  printf "\
+Usage: $0 area
+"
+  exit
+fi
+
+if [ "$1" = "area" ]; then
   outfile=$(mktemp --tmpdir=/tmp --suffix=.png snapshot_XXXXX)
   gnome-screenshot --file=$outfile --area
   copyq copy image/png - < $outfile
   notify-send "Saved to $outfile"
   exit 0
 fi
-
-
