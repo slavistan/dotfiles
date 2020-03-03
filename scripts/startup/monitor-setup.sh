af0191cb7392d639cb6671efc2699ab03b79fccf
Stanislaw Hüll <st@nislaw.de>
2020-03-03 14:34:44 +0100

    stuff

--- scripts/startup/monitor-setup.sh
+++ scripts/startup/monitor-setup.sh
@@ -8,14 +8,14 @@ if [ "$(hostname -s)" = "Berenice" ]; then
   OFFICE="DP-1-1-8\nDP-1-2\neDP-1"
   SOLO="eDP-1"
 
-  if [ -z $(comm -3 <(echo "$mons") <(echo "$OFFICE")) ]; then
+  if [ -z "$(comm -3 <(echo "$mons") <(echo "$OFFICE"))" ]; then
     notify-send "Monitors: 🖳 🖵 🖵 " &
     xrandr --output "DP-1-2" --off
     xrandr --output "DP-1-1-8" --off
     xrandr --auto
     xrandr --output "DP-1-2" --right-of "eDP-1"
     xrandr --output "DP-1-1-8" --right-of "DP-1-2"
-  elif [ -z $(comm -3 <(echo "$mons") <(echo "$SOLO")) ]; then
+  elif [ -z "$(comm -3 <(echo "$mons") <(echo "$SOLO"))" ]; then
     notify-send "Monitors: 🖳" &
     xrandr --auto
   else
