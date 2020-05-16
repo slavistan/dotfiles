f147041e83bfbc7aa5c0a4ceddb7a8f692903b1a
Stanislaw Hüll <st@nislaw.de>
2020-05-16 09:46:01 +0200

    Pretty formatting

--- scripts/startup/autostart.sh
+++ scripts/startup/autostart.sh
@@ -4,6 +4,7 @@
 
 source ~/.profile
 
+
 ## Start sxhkd or, if running, reload config
 
 if [ -z "$(pgrep sxhkd)" ]; then
@@ -12,15 +13,18 @@ else
   kill -SIGUSR1 "$(pgrep sxhkd)"
 fi
 
+
 ## Start copyq daemon
 
 if [ -z "$(pgrep copyq)" ]; then
   copyq &
 fi
 
+
 ## Start dropbox daemon
 
 dropbox start &
+
 
 ## Start compton (required for transparency)
 
@@ -28,9 +32,11 @@ if [ -z "$(pgrep compton)" ]; then
   compton &
 fi
 
+
 ## Run dwm status bar script
 
 $DOTFILES/scripts/dwm-status.sh &
+
 
 ## Set background image
 
@@ -40,22 +46,26 @@ else
   feh --bg-scale $DOTFILES/files/img/ubuntu-teal.jpg
 fi
 
-# Load custom keyboard layout
+
+## Load custom keyboard layout
 
 setxkbmap -layout k6
 
-# Run monitor setup
+
+## Run monitor setup
 
 if [ -f "$DOTFILES/scripts/startup/monitor-setup.sh" ]; then
   $DOTFILES/scripts/startup/monitor-setup.sh
 fi
 
-# Run machine-specific startup script
+
+## Run machine-specific startup script
 
 if [ -f "$DOTFILES/scripts/startup/machine-specific.sh" ]; then
   $DOTFILES/scripts/startup/machine-specific.sh
 fi
 
-# Display log
+
+## Display log
 
 libreoffice ~/Dropbox/log.ods &
