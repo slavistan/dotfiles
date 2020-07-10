e477698fe3059c496198c261b470ae1188d9050c
Stanislaw Hüll <huell@stud.uni-heidelberg.de>
2020-07-10 16:10:45 +0200

    jupyter update

--- jupyter/install-jupyter.zsh
+++ jupyter/install-jupyter.zsh
@@ -10,17 +10,32 @@ Usage:
   elif [ -z "$(command -v jupyter)" ] || [ "$1" = "--force" ]; then
     loglnprefix "jupyter" "Installing 'jupyter' ..."
 
-    # vi-bindings for cells
-    please pip3 install --upgrade jupyterlab
-    please jupyter labextension install @axlair/jupyerlab_vim
+    # jupyterlab (& ipython kernel)
+    # TODO(fix): Install packages into system dir (NOT root's home)
+    #   pip3 has some --sys-dir flag. What about R?
+    please pip3 install jupyterlab
 
-    # Table of Contents
+    # R kernel
+    please R -q -e 'install.packages("IRkernel")'
+    R -q -e 'IRkernel::installspec(user=F, prefix="/usr/local/")'
+
+    # zsh kernel
+    please pip3 install zsh_jupyter_kernel
+    please python3 -m zsh_jupyter_kernel.install --sys-prefix
+
+    # bash kernel
+    please pip3 install bash_kernel
+    please python3 -m bash_kernel.install
+
+    # sos kernel
+    please pip3 install sos-notebook
+    please python3 -m sos_notebook.install
+    please jupyter labextension install transient-display-data
+    please jupyter labextension install jupyterlab-sos
+
+    # jupyterlab extensions
+    please jupyter labextension install @axlair/jupyterlab_vim
     please jupyter labextension install @jupyterlab/toc
-
-    # sos
-    please jupyter labextension install jupyerlab-sos
-
-    # TODO(feat): Install jupyterlab kernels
 
     loglnprefix "jupyter" "... done installing 'jupyter'."
   else
