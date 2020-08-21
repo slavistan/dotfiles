abd3aeb6697599609b48c2821bb21c00728293f2
Stanislaw Hüll <st@nislaw.de>
2020-08-21 12:31:41 +0200

    Cleanup jupyter/

--- jupyter/install-jupyter.zsh
+++ jupyter/install-jupyter.zsh
@@ -1,42 +1,66 @@
 __install_jupyter() {
-  if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-    printf "\
-Usage:
-  $0 [--force]
+  case "$1" in
+    -h) cat <<-EOF
+			Usage:
+			  $0 -h  - Show this help
+			  $0 -u  - Update existing installation
+			
+			  Install jupyter, jupyterlab, assorted kernels and plugins.
+			EOF
+      ;;
+    -u)
+      please pip3 install --upgrade jupyterlab
+      please R -q -e 'install.packages("IRkernel")'
+      R -q -e 'IRkernel::installspec(user=F, prefix="/usr/local/")'
 
-  Install jupyter, jupyterlab, assorted kernels and plugins.
-"
-    exit 0
-  elif [ -z "$(command -v jupyter)" ] || [ "$1" = "--force" ]; then
-    loglnprefix "jupyter" "Installing 'jupyter' ..."
+      please pip3 install --upgrade zsh_jupyter_kernel
+      please python3 -m zsh_jupyter_kernel.install --sys-prefix
 
-    # jupyterlab (& ipython kernel)
-    please pip3 install jupyterlab
+      please pip3 install --upgrade bash_kernel
+      please python3 -m bash_kernel.install
 
-    # R kernel
-    please R -q -e 'install.packages("IRkernel")'
-    R -q -e 'IRkernel::installspec(user=F, prefix="/usr/local/")'
+      please pip3 install --upgrade sos-notebook
+      please python3 -m sos_notebook.install
+      please jupyter labextension install transient-display-data
+      please jupyter labextension install jupyterlab-sos
 
-    # zsh kernel
-    please pip3 install zsh_jupyter_kernel
-    please python3 -m zsh_jupyter_kernel.install --sys-prefix
+      please jupyter labextension install @axlair/jupyterlab_vim
+      please jupyter labextension install @jupyterlab/toc
 
-    # bash kernel
-    please pip3 install bash_kernel
-    please python3 -m bash_kernel.install
+      please jupyter lab build
+      ;;
+    *)
+      loglnprefix "jupyter" "Installing 'jupyter' ..."
 
-    # sos kernel
-    please pip3 install sos-notebook
-    please python3 -m sos_notebook.install
-    please jupyter labextension install transient-display-data
-    please jupyter labextension install jupyterlab-sos
+      # jupyterlab (& ipython kernel)
+      please pip3 install jupyterlab
 
-    # jupyterlab extensions
-    please jupyter labextension install @axlair/jupyterlab_vim
-    please jupyter labextension install @jupyterlab/toc
+      # R kernel
+      please R -q -e 'install.packages("IRkernel")'
+      R -q -e 'IRkernel::installspec(user=F, prefix="/usr/local/")'
 
-    loglnprefix "jupyter" "... done installing 'jupyter'."
-  else
-    loglnprefix "jupyter" "Nothing to do."
-  fi
+      # zsh kernel
+      please pip3 install zsh_jupyter_kernel
+      please python3 -m zsh_jupyter_kernel.install --sys-prefix
+
+      # bash kernel
+      please pip3 install bash_kernel
+      please python3 -m bash_kernel.install
+
+      # sos kernel
+      please pip3 install sos-notebook
+      please python3 -m sos_notebook.install
+      please jupyter labextension install transient-display-data
+      please jupyter labextension install jupyterlab-sos
+
+      # jupyterlab extensions
+      please jupyter labextension install @axlair/jupyterlab_vim
+      please jupyter labextension install @jupyterlab/toc
+
+      loglnprefix "jupyter" "... done installing 'jupyter'."
+      ;;
+  esac
+
+  exit
+
 }
