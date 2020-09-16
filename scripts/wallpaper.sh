1a6a7ccc51ad94e8da9d48ce23ee0d35716ebfbb
Stanislaw Hüll <st@nislaw.de>
2020-09-16 09:33:40 +0200

    Fix wallpaper script

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env sh
 
 WALLPAPERS=~/dat/img/wallpaper/
-DISPATCHER=~/dat/img/wall
+DISPATCHER=~/dat/img/wall # symlinked to currently active background image
+
+_ls() {
+  if [ "$1" = "-0" ]; then
+    find "$WALLPAPERS" -type f -print0
+  else
+    find "$WALLPAPERS" -type f
+  fi
+}
+
+_reload() {
+  feh --bg-scale "$DISPATCHER"
+}
 
 case "$1" in
   -r|--random)
     [ -d "$WALLPAPERS" ] || exit 1
-    new="$(find "$WALLPAPERS" -type f -print0 | shuf -z | head -1 -z | tr -d '\0')"
-    ln -sf "$(realpath $new)" "$(realpath $DISPATCHER)"
-    exec $0
+    new="$(_ls -0 | shuf -z | head -1 -z | tr -d '\0')"
+    cd "$(realpath "$(dirname $DISPATCHER)")"
+    ln -sf "$(realpath $new)" "$(basename $DISPATCHER)"
+    _reload
     ;;
-  *) # reload current
-    feh --bg-scale "$DISPATCHER"
+  -l|--list)
+    shift
+    _ls "$@"
     ;;
 esac
-
