87a5ae48f5103ccdada6889230493a498262bd03
Stanislaw Hüll <st@nislaw.de>
2020-12-27 15:43:29 +0100

    dmenyt has nice previews

--- scripts/dmenyt.sh
+++ scripts/dmenyt.sh
@@ -12,22 +12,26 @@ if [ -z "$query" ]; then
 	query="$(echo "" | dmenu -p "Search YT Video:")" || exit 1
 fi
 query="$(echo "$query" | sed 's/ /+/g')"
-echo "Search query: $query"
 
 reply="$(curl -s "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$query&type=video&maxResults=$maxresults&key=$YT_API_KEY")"
 
 tmpdir="$(mktemp -d -t dmenyt-thumbnails-XXXXXX)"
-echo "tmpdir = '$tmpdir'"
 
 # dl thumbnails
-printf "Downloading thumbnails ... "
 thumbnailurls="$(echo "$reply" | jq -r '.items[].snippet.thumbnails.high.url')"
 videoids="$(echo "$reply" | jq -r '.items[].id.videoId')"
+titles="$(echo "$reply" | jq -r '.items[].snippet.title')"
 cd "$tmpdir"
 paste -d '\n' <(echo "$videoids") <(echo "$thumbnailurls") |
 	xargs -n 2 -P 8 curl -s -o
-cd -
-echo "done."
+cd - >/dev/null
+
+# save titles to .<videoid>.title
+paste -d ' ' <(echo "$videoids") <(echo "$titles") | while read line; do
+	videoid="$(echo "$line" | cut -d' ' -f1)"
+	title="$(echo "$line" | cut -d' ' -f2-)"
+	echo "$title" > "$tmpdir/.$videoid.title"
+done
 
 # TODO: Howto set the returncode for lf?
 # An aborted selection should be indicated via the return code like dmenu does.
@@ -36,7 +40,7 @@ videoid="$(lf-thumbnailselect.sh "$tmpdir")"
 rm -r "$tmpdir"
 if [ ! -z "$videoid" ]; then
 	dwmswallow $WINDOWID
-	mpv "https://youtu.be/$videoid"
+	mpv "https://youtu.be/$videoid" >/dev/null 2>&1
 else
 	exit 1
 fi
