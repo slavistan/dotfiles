389749eee60747c148c0545eaf0a094ebfd84a0a
Stan <st@nislaw.de>
2021-05-13 15:14:37 +0200

    stuff

--- scripts/snapshot.sh
+++ scripts/snapshot.sh
@@ -1,31 +1,29 @@
 #!/usr/bin/env sh
 
 if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-  printf "\
+	printf "\
 Usage:
-  (1) $0 --select-area | -s
+	(1) $0 --select-area | -s
 
 Takes a snapshot of your desktop.
 "
-  exit
+	exit
 fi
 
 case "$1" in
-  -s|--select-area)
-    tmpdir=$(mktemp -p /tmp/ -d snapshot_XXXXXX)
-    outfile="$tmpdir/snapshot.png"
-    import "$outfile" &&
-      # TODO(fix): pngs are pale inside anki when using xclip
-      xclip -t image/png -selection clipboard "$tmpdir/snapshot.png"
-      # copyq copy image/png - < "$tmpdir/snapshot.png"
-    action=$(dunstify \
-      -I "$outfile" \
-      --timeout=5000 \
-      --action="default,Save to:" \
-      "Snapshot")
-    if [ "$action" = "default" ]; then
-      # TODO(feat): show lf save-file dialog
-      spacefm "$tmpdir" &
-    fi
-    ;;
+	-s|--select-area)
+		tmpdir=$(mktemp -p /tmp/ -d snapshot_XXXXXX)
+		outfile="$tmpdir/snapshot.png"
+		import "$outfile" &&
+			# FIXME: pngs are pale inside anki when using xclip
+			# xclip -t image/png -selection clipboard "$tmpdir/snapshot.png"
+			copyq copy image/png - < "$tmpdir/snapshot.png"
+		if saveto="$(lf-saveas -p "Save as:" -t f -s ~ -x)"; then
+			cp "$outfile" "$saveto"
+			dunstify -I "$saveto" -t 5000
+		fi
+		;;
 esac
+
+# snapchad:
+#  - cache previous save-to directory
