b2d9b6d4edf8bd9a92407b9cf0e1ee82b4120243
Stan <st@nislaw.de>
2021-08-29 12:42:37 +0200

    stuff

---
+++ install_new.sh
@@ -0,0 +1,19 @@
+# zsh installation
+
+- zsh installieren
+- chsh
+- .zprofile symlink,
+- .profile symlink, (vielleicht vorher)
+- plugin: zsh-syntax-highlighting
+- plugins: lf-shellcd
+- symlink ~/.config/zsh
+
+# lf setup
+- install lf (github)/aur
+- TODO: Single setup ohne verteilte Dateien
+
+# xorg setup
+- install xorg + libs, xinit
+- install dwm
+- install st
+- symlink ~/.xinitrc
